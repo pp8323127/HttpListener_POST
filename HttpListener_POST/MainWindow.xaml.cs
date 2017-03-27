b9f5b93b499062310d500a6d94072716d1371325
CHUANG YUN HAO <pp832312@gmail.com>
2017-03-27 18:25:37 +0800

    init

---
+++ HttpListener_POST/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace HttpListener_POST
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpListener listener;
+        private Thread listenThread1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8000/");
+            listener.Prefixes.Add("http://127.0.0.1:8000/");
+            listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+
+            listener.Start();
+            this.listenThread1 = new Thread(new ParameterizedThreadStart(startlistener));
+            listenThread1.Start();
+
+        }
+
+        private void startlistener(object s)
+        {
+            while (true)
+            {
+                ////blocks until a client has connected to the server
+                ProcessRequest();
+            }
+        }
+
+        private void ProcessRequest()
+        {
+            var result = listener.BeginGetContext(ListenerCallback, listener);
+            result.AsyncWaitHandle.WaitOne();
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            var context = listener.EndGetContext(result);
+            Thread.Sleep(1000);
+            var data_text = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
+
+            //functions used to decode json encoded data.
+            //JavaScriptSerializer js = new JavaScriptSerializer();
+            //var data1 = Uri.UnescapeDataString(data_text);
+            //string da = Regex.Unescape(data_text);
+            // var unserialized = js.Deserialize(data_text, typeof(String));
+
+            var cleaned_data = System.Web.HttpUtility.UrlDecode(data_text);
+
+            context.Response.StatusCode = 200;
+            context.Response.StatusDescription = "OK";
+
+            //use this line to get your custom header data in the request.
+            //var headerText = context.Request.Headers["mycustomHeader"];
+
+            //use this line to send your response in a custom header
+            //context.Response.Headers["mycustomResponseHeader"] = "mycustomResponse";
+
+            MessageBox.Show(cleaned_data);
+            context.Response.Close();
+        }
+
+
+    }
+}
